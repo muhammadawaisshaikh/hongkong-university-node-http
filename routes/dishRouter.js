73a413811c85c03ee70ab773fa1d0990ec245e4e
Muhammad Awais <awaisshaikh94@gmail.com>
2019-12-03 18:43:45 +0500

    dishRouter have support of /:dishId

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -5,24 +5,45 @@ const dishRouter = express.Router();
 dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req, res, next) => {
-    res.end('Will send all dishes to you!');
-})
-.post((req, res, next) => {
-    res.end('Will add the dish ' + req.body.name);
-})
-.put((req, res, next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /dishes');
-})
-// dangerous operation 
-.delete((req, res, next) => {
-    res.end('Deleting all dishes');
-});
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        next();
+    })
+    .get((req, res, next) => {
+        res.end('Will send all dishes to you!');
+    })
+    .post((req, res, next) => {
+        res.end('Will add the dish ' + req.body.name);
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /dishes');
+    })
+    // dangerous operation 
+    .delete((req, res, next) => {
+        res.end('Deleting all dishes');
+    });
+
+dishRouter.route('/:dishId')
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        next();
+    })
+    .get((req, res, next) => {
+        res.end('Will send the details of the dish:' + req.params.dishId);
+    })
+    .post((req, res, next) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on /dishes/' + req.params.dishId);
+    })
+    .put((req, res, next) => {
+        res.end('Will update the dish:' + req.params.dishId);
+    })
+    // dangerous operation 
+    .delete((req, res, next) => {
+        res.end('Deleting the dish:' + req.params.dishId);
+    });
 
 module.exports = dishRouter;
